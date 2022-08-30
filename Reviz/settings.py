e43dc2c5e3d3dee0028df563994e2c1b325d85dc
roman <roman.mariancik@gmail.com>
2022-08-30 19:10:13 +0000

    ubuntu

--- Reviz/settings.py
+++ Reviz/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'NAME': "Reviz",
         'USER': 'django',
         'PASSWORD': 'M22ercedes!',
-        'HOST': '192.168.1.12',
+        'HOST': '127.0.0.1',
         'PORT': ''
     }
 }

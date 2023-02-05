3a6afec987392ef65dbf63330a6d20ee5ab4a859
roman <roman.mariancik@gmail.com>
2023-02-06 00:34:52 +0100

    reopening reviz

--- run.sh
+++ run.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 build=false
 superuser=false
+down=false
+up=false
 
-while getopts ":bs" option; do
+while getopts ":bsdu" option; do
   case $option in
     h) echo "usage: $0 [-h] [-b] [-s]"; exit ;;
     b) build=true ;;
     s) superuser=true ;;
+    d) down=true ;;
+    u) up=true ;;
     ?) echo "error: option -$OPTARG does not exist - specify -b if you want to build for the first time or -s if you want to create a superuser"; exit ;;
   esac
 done
@@ -19,6 +23,15 @@ fi
 
 if [ "$superuser" = true ] ; then
   docker-compose run app python3 manage.py createsuperuser
+
 fi
 
-docker-compose up
+if [ "$up" = true ] ; then
+  docker-compose up
+fi
+
+if [ "$down" = true ] ; then
+  docker-compose down
+fi
+
+
